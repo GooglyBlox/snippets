5434406f6b512df1b01567b12d2695939f6aa774
Google <53668973+GooglyBlox@users.noreply.github.com>
2023-02-07 19:42:48 -0800

    Update removemissing.js

--- scripts or something/removemissing.js
+++ scripts or something/removemissing.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Remove the Missing element
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/GooglyBlox/userscripts/blob/main/scripts%20or%20something/removemissing.js
 // @version      0.1
 // @description  Remove the "Missing" element from the current webpage on Canvas when you press backslash. Not automated because its actually useful to know what's missing, but not so nice to see a giant red missing button all the time.
 // @author       GooglyBlox_Improved#0001
