18e7a7dcffb7f802b84063063f1eef79470a4d23
Google <53668973+GooglyBlox@users.noreply.github.com>
2023-02-07 20:11:44 -0800

    modified the way it removes tags
    "Late" is now automatically removed, while "Missing" is removed after you press backslash.

--- scripts or something/removemissing.js
+++ scripts or something/removemissing.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove the Missing or Late element
 // @namespace    https://github.com/GooglyBlox/userscripts/blob/main/scripts%20or%20something/removemissing.js
-// @version      0.2
+// @version      0.6
 // @description  Remove the "Missing" or "Late" element from the current webpage on Canvas when you press backslash. Not automated because its actually useful to know what's missing, but not so nice to see a giant red missing button all the time.
 // @author       GooglyBlox_Improved#0001
 // @match        https://*.instructure.com/*
@@ -11,10 +11,16 @@
 document.addEventListener('keydown', function(event) {
   if (event.key === '\\') {
     const element1 = document.querySelector('.dLyYq_bGBk.dLyYq_zGXc');
-    const element2 = document.querySelector('.dLyYq_bGBk.dLyYq_pypk');
-    const element = element1 || element2;
-    if (element) {
-      element.remove();
+    if (element1) {
+      element1.remove();
     }
   }
 });
+
+const checkForElement = setInterval(function() {
+  const element2 = document.querySelector('.dLyYq_bGBk.dLyYq_pypk');
+  if (element2) {
+    element2.remove();
+    clearInterval(checkForElement);
+  }
+}, 100);
