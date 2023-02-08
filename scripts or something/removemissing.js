4cbd3db6f6176ac91d258ebba2b7dd0ce3058d9e
Google <53668973+GooglyBlox@users.noreply.github.com>
2023-02-07 20:01:13 -0800

    added the "late" tag too

--- scripts or something/removemissing.js
+++ scripts or something/removemissing.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Remove the Missing element
+// @name         Remove the Missing or Late element
 // @namespace    https://github.com/GooglyBlox/userscripts/blob/main/scripts%20or%20something/removemissing.js
-// @version      0.1
-// @description  Remove the "Missing" element from the current webpage on Canvas when you press backslash. Not automated because its actually useful to know what's missing, but not so nice to see a giant red missing button all the time.
+// @version      0.2
+// @description  Remove the "Missing" or "Late" element from the current webpage on Canvas when you press backslash. Not automated because its actually useful to know what's missing, but not so nice to see a giant red missing button all the time.
 // @author       GooglyBlox_Improved#0001
 // @match        https://*.instructure.com/*
 // @grant        none
@@ -10,7 +10,9 @@
 
 document.addEventListener('keydown', function(event) {
   if (event.key === '\\') {
-    const element = document.querySelector('.dLyYq_bGBk.dLyYq_zGXc');
+    const element1 = document.querySelector('.dLyYq_bGBk.dLyYq_zGXc');
+    const element2 = document.querySelector('.dLyYq_bGBk.dLyYq_pypk');
+    const element = element1 || element2;
     if (element) {
       element.remove();
     }
